d316ebd9ac235ab0a8120029d6914b5ba5b353f7
Magnus031 <130850716+Magnus031@users.noreply.github.com>
2023-07-21 18:06:16 +0800

    Delete Misc/Lab1 directory

--- Misc/Lab1/count.py
+++
@@ -1,3 +0,0 @@
-text='ur {gY ),yPT	Yb;85SY8 o:|k\'y^> %n` f@XP b	zbJY?] 8Q" [CP	|;o {gY w*)`qN\n8 28S1}Q'
-#text='By the mid-nin^;a/3j8 oent\'i^, the =erm "ic^J5D  8Q" enP{|ed the AmericaQ'
-print(len(text))
